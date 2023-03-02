bff5729935d3bdbeca031dbf062b991315937a43
sumitsetia <summitsetia@gmail.com>
2023-03-03 11:31:02 +1300

    Choose RAM

--- main.py
+++ main.py
@@ -2,13 +2,20 @@ print ("Budget Laptop Calculator")
 
 type_list = ['gaming', 'programming', 'working', 'editing']
 while True:
-    user_choice = input("Please choose one of the following options: " + ", ".join(type_list) + '  ')
-    if user_choice in type_list:
+    type_choice = input("Please choose one of the following options: " + ", ".join(type_list) + '  ')
+    if type_choice in type_list:
+        print("You chose", type_choice)
         break
     else:
         print("Invalid choice. Please try again.")
 
-print("You chose", user_choice)
 
 ram_list = ['4GB', '8GB', '16GB', '32GB']
-while true:
+while True:
+    ram_choice = input("Please choose one of the following options:" + ", ".join(ram_list) + ' ')
+    if ram_choice in ram_list:
+        print("You chose", ram_choice)
+        break
+    else:
+        print("Invalid choice. Please try again.")
+
