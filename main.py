0e1d604922d69e88c586b1f8e225fe914d694d4e
sumitsetia <summitsetia@gmail.com>
2023-03-03 18:51:00 +1300

    Choose Storage

--- main.py
+++ main.py
@@ -1,5 +1,11 @@
 print ("Budget Laptop Calculator")
 
+
+# trial reading information from an existing csv file locally store
+# trial reading a file that gets updated remotely online
+# trial using an API with this information  - example Google Map API, chatGPT API
+# webscraping using BeautifulSoup library in Python - select a website, store information in a file
+
 type_list = ['gaming', 'programming', 'working', 'editing']
 while True:
     type_choice = input("Please choose one of the following options: " + ", ".join(type_list) + '  ')
@@ -15,6 +21,15 @@ while True:
     ram_choice = input("Please choose one of the following options:" + ", ".join(ram_list) + ' ')
     if ram_choice in ram_list:
         print("You chose", ram_choice)
+        break
+    else:
+        print("Invalid choice. Please try again.")
+
+storage_list = ['256GB', '512GB', '1TB']
+while True:
+    storage_choice = input("Please choose one of the following options:" + ", ".join(storage_list) + ' ')
+    if storage_choice in storage_list:
+        print("You chose", storage_choice)
         break
     else:
         print("Invalid choice. Please try again.")
